47e185ecbc2c28201facf335d5b4b4ff7aa7ada8
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2025-10-23 15:47:07 +0200

    Created an example of the setting flex: auto function

---
+++ 1. Foundations Course/3. Flexbox/Setting flex; auto/javascript.js
@@ -0,0 +1,13 @@
+const flexAutoItem = document.getElementById("flex-auto");
+const defaultItem = document.getElementById("default");
+flexAutoItem.addEventListener("click", () => { 
+    defaultItem.style.display = 
+    defaultItem.style.display === "none" ? "block" : "none";
+});
+
+
+
+
+
+
+
