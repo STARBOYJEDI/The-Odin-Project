ff4d8bd2634474c287669b2e73bc5ca34c89c6cd
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-01-06 20:11:09 +0200

    Add JavaScript functions for favorite animals and anime
    - Created functions to return favorite animal and anime messages.
    - Added console logs to display the messages.
    - Initialized empty HTML and CSS files for future development.

---
+++ 1. Foundations Course/4. JavaScript Basics/3. Function Basics/3.1 Functions/functions.js
@@ -0,0 +1,28 @@
+function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!";
+}
+const message = favoriteAnimal('Cat')
+console.log(message);
+
+// Another example but the const message command is omitted
+function favoriteAnimal2(animal2) {
+    return animal2 + " is my second favorite animal!";
+}
+console.log(favoriteAnimal2('Dog'));
+
+function favoriteAnime(anime) {
+    return anime + " is my favorite anime!";
+}
+console.log(favoriteAnime('Fullmetal Alchemist: Brotherhood'));
+
+
+
+
+
+
+
+
+
+
+
+
