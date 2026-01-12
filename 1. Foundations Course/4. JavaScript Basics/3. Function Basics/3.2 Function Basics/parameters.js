e705e423a3161d01c27751cb0ec844f1e67ef158
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-01-12 14:52:30 +0200

    Refactor function names and enhance parameter examples in parameters.js

--- 1. Foundations Course/4. JavaScript Basics/3. Function Basics/3.2 Function Basics/parameters.js
+++ 1. Foundations Course/4. JavaScript Basics/3. Function Basics/3.2 Function Basics/parameters.js
@@ -1,16 +1,27 @@
 // We can pass arbitrary data to functions using parameters.
 
-function showMessage1(from, text) { // parameters: from, text
+function showMessage5(from, text) { // parameters: from, text
     alert(from + ': ' + text);
 }
 
-showMessage1('Ann', 'Hello!'); // Ann: Hello! (*)
-showMessage1('Ann', "What's up?"); // Ann: What's up? (**)
+showMessage5('Ann', 'Hello!'); // Ann: Hello! (*)
+showMessage5('Ann', "What's up?"); // Ann: What's up? (**)
+// When the function is called in lines (*), (**), the given values are copied to the local variables from and text.
 
+//Here’s one more example: we have a variable from and pass it to the function. Please note: the function changes from, but the change is not seen outside, because a function always gets a copy of the value:
+function showMessage6(from, text) {
 
+    from = '*' + from + '*'; // make "from" look nicer
 
+    alert( from + ': ' + text );
+}
 
+let from = "Ann";
 
+showMessage6(from, "Hello"); // *Ann*: Hello
+
+// the value of "from" is the same, the function modified a local copy
+alert( from ); // Ann
 
 
 
