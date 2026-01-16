5f621c3a61b199ed7945e7810731ceef45dec8ff
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-01-16 18:52:22 +0200

    2. Created a function that multiplies 2 numbers and returns their products.

--- 1. Foundations Course/4. JavaScript Basics/3. Function Basics/3.3 Assignment/javascript.js
+++ 1. Foundations Course/4. JavaScript Basics/3. Function Basics/3.3 Assignment/javascript.js
@@ -1,10 +1,14 @@
+// 1. Write a function called add7 that takes a number as a parameter and returns that number + 7.
 function add7(num) {
     return num + 7;
 }
 console.log(add7(10)); 
 
-
-
+// 2. Write a function called multiply that takes 2 numbers and returns their product.
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiply(3, 4));
 
 
 
