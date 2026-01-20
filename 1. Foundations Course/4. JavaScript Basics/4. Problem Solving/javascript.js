ee03c218d6442314fac361914589f58844d982f8
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-01-20 13:18:07 +0200

    Implement FizzBuzz logic and user input prompt in JavaScript

--- 1. Foundations Course/4. JavaScript Basics/4. Problem Solving/javascript.js
+++ 1. Foundations Course/4. JavaScript Basics/4. Problem Solving/javascript.js
@@ -1,4 +1,13 @@
-
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+// The above code should create a little popup box that ask the user for a number. 
+// The input we get back will be stored in our variable 'answer'.
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    } else {
+        console.log(i);
+    }
+}
 
 
 
