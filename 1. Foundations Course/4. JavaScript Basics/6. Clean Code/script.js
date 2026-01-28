689bd75c68f63be3469169aac502a49bb9e1351b
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-01-28 18:10:16 +0200

    Clean code

---
+++ 1. Foundations Course/4. JavaScript Basics/6. Clean Code/script.js
@@ -0,0 +1,24 @@
+// What is clean code?
+// Consider the following examples:
+// Example A:
+const x= function (z){
+    const w = "Hello ";
+return w + z
+
+}
+
+x("John");
+
+// Example B:
+const generateUserGreeting = function (name) {
+    const greeting = "Hello ";
+    return greeting + name;
+};
+
+generateUserGreeting("John");
+
+
+
+
+
+
