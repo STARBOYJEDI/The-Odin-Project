f19910c208ac78edb71f890779b0d0f1e10039de
Mbuso Mabuza <mbusomabuza0613@gmail.com>
2026-02-03 19:25:48 +0200

    Add initial HTML and JavaScript files for DOM manipulation examples
    - Created index.html and js-file.js for the code review section, including a basic structure and a script to manipulate the DOM.
    - Added example HTML and script files demonstrating various DOM manipulation techniques, including style changes and class management.

---
+++ 1. Foundations Course/4. JavaScript Basics/8. DOM Manipulation and Events/code review/js-file.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
+
+
+
+
+
+
+
+
+
+
+
+
